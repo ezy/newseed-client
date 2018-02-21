a9f1f035b5d860d2a4242c16071fc7603102d7f5
Ezra Keddell <ezrakeddell@gmail.com>
2018-02-21 15:34:37 +1300

    add conditional for anchor link to location

--- app/components/nav-header.js
+++ app/components/nav-header.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
+import { getOwner } from '@ember/application';
+import { set } from '@ember/object'
 
 export default Component.extend({
+  init() {
+    this._super(...arguments);
+    let owner = getOwner(this).lookup('router:main').get('currentRouteName');
+    if (owner === 'index') {
+      set(this, 'isIndex', true);
+    }
+  },
+
+  isIndex: false
 });
