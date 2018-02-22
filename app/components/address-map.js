c80e6a31e90ef9c5e1c340e59fdf381cd9833cd0
Ezra Keddell <ezrakeddell@gmail.com>
2018-02-22 15:01:25 +1300

    format contact page with form

---
+++ app/components/address-map.js
@@ -0,0 +1,5 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  location: [-41.230087, 174.894357],
+});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,6 @@ import { get } from '@ember/object';
 import { filter } from '@ember/object/computed';
 
 export default Controller.extend({
-  location: [-41.230087, 174.894357],
   slides: filter('notices', (notice) => {
     let tags = get(notice, 'tags');
     return tags.includes('slide');
