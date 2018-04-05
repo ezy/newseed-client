b1f9cd92dd8c59ee08e25818fc33f75c4813ad83
Ezra Keddell <ezrakeddell@gmail.com>
2018-04-05 15:07:31 +1200

    set media page

--- app/components/media-audio.js
+++ app/components/media-audio.js
@@ -1,7 +1,13 @@
 import Component from '@ember/component';
 import { sort } from '@ember/object/computed';
+import $ from 'jquery';
 
 export default Component.extend({
+
+  didRender() {
+    this._super(...arguments);
+    $('[data-toggle="tooltip"]').tooltip();
+  },
 
   audioSortingDesc: Object.freeze(['date:desc']),
   sortedAudio: sort('audios', 'audioSortingDesc')
