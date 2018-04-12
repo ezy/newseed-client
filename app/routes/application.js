a628d8e1c19554a0ec17349ec5a29132654c04db
Ezra Keddell <ezrakeddell@gmail.com>
2018-04-12 13:29:58 +1200

    add loader

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,23 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
+
+  session: service(),
+
   setupController(controller) {
     this.get('store').findRecord('church', 0).then(function(church) {
       controller.set('church', church);
     });
-  }
+  },
+
+  // beforeModel() {
+  //   if (!this.get('session').session.isAuthenticated) {
+  //     this.transitionTo('login');
+  //   }
+  //   else {
+  //     this.transitionTo('index');
+  //   }
+  // }
 });
