f321542ce932d9bb8a7acdc3e31288261db0ca6f
Ezra Keddell <ezrakeddell@gmail.com>
2018-04-19 11:36:55 +1200

    update json structure and about pages

--- app/routes/about.js
+++ app/routes/about.js
@@ -2,4 +2,9 @@ import Route from '@ember/routing/route';
 import SetChurchController from 'mustard/mixins/church-set-controller';
 
 export default Route.extend(SetChurchController, {
+  setupController(controller) {
+    this.get('store').findAll('people').then(function(people) {
+      controller.set('people', people);
+    });
+  }
 });
