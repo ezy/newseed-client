db854b1230d8a490a406e0a67adbd4d39eef1a5a
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-02 15:42:22 +1200

    adjust people formatting and typography

--- app/models/people.js
+++ app/models/people.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  copy: DS.attr('string'),
+  image: DS.attr('string'),
+  text: DS.attr('string'),
 
   churches: DS.belongsTo('churches')
 });
