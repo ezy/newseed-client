3907dad5d8c32e3a2ad9c8c22c1ca3991b24ad37
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-08 11:35:18 +1200

    add stock images

--- app/controllers/forgot-password.js
+++ app/controllers/forgot-password.js
@@ -6,14 +6,12 @@ export default Controller.extend({
   init() {
     this._super(...arguments);
     this.set('email', null);
-    console.log(this.get('firebaseApp').auth());
   },
   actions: {
     resetPassword(email) {
-      console.log(email);
       this.get('firebaseApp').auth().sendPasswordResetEmail(email)
         .then(() => {
-          this.get('flashMessages').success('If email exists the password reset email has been sent');
+          this.get('flashMessages').success('The password reset email has been sent');
         })
         .catch(err => {
           this.get('flashMessages').danger(err.message);
