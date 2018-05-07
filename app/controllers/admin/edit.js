a8b64afc22ae3962eb1a0a0e632208a1be4034f0
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-07 13:38:53 +1200

    get edit working

--- app/controllers/admin/edit.js
+++ app/controllers/admin/edit.js
@@ -1,4 +1,23 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  init() {
+    this._super(...arguments);
+    this.set('isSaving', false);
+  },
+  actions: {
+    saveContent() {
+      let model = this.get('model');
+      this.set('isSaving', true);
+      model.save()
+        .then(() => {
+          this.set('isSaving', false);
+          console.log('saved',model);
+        })
+        .catch(() => {
+          this.set('isSaving', false);
+          console.log('something went wrong');
+        });
+    }
+  }
 });
