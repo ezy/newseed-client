e2a5e6afd5b2939b4c0c9c1a03dc70bae2ad9c1a
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-10 19:28:55 +1200

    setup display of sunday service

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,38 @@
 import Controller from '@ember/controller';
-import { get } from '@ember/object';
+import { get, computed } from '@ember/object';
 import { filter } from '@ember/object/computed';
+import moment from 'moment';
 
 export default Controller.extend({
-  filteredNotice: filter('notices', (notice) => {
+  filteredNotice: filter('model.notices', function(notice) {
     return notice.get('status') === 'published';
   }),
-  filteredAudio: filter('audios', (audio) => {
+  filteredAudio: filter('model.audios', function(audio) {
     return audio.get('status') === 'published';
   }),
-  slides: filter('filteredNotice', (notice) => {
+  slides: filter('filteredNotice', function(notice) {
     let tags = get(notice, 'tags');
     return tags.includes('slide');
   }),
-  newsFeed: filter('filteredNotice', (notice) => {
+  newsFeed: filter('filteredNotice', function(notice) {
     let tags = get(notice, 'tags');
     return !tags.includes('slide');
-  })
+  }),
+  nextService: computed('model.services', function() {
+    let services = this.get('model.services'),
+        now = moment(),
+        display = null,
+        // set the checkDate to next Sunday just before midnight
+        checkDate = moment().day(7).endOf('day');
+    services.forEach(service => {
+      let date = get(service, 'date');
+      // Service date needs to be after time now, and same or before the checkDate
+      if (moment(date).isAfter(now, 'hour') && moment(date).isSameOrBefore(checkDate, 'minute')) {
+        // because date is before checkDate we update checkDate for the next compare
+        checkDate = moment(date);
+        display = service;
+      }
+    });
+    return display;
+  }),
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,13 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
-  setupController(controller) {
-    this.get('store').findAll('audio').then(function(audio) {
-      controller.set('audios', audio);
-    });
-    this.get('store').findAll('notice').then(function(notice) {
-      controller.set('notices', notice);
-    });
-    this.get('store').findRecord('church', 0).then(function(church) {
-      controller.set('church', church);
+  model() {
+    return RSVP.hash({
+      audios: this.get('store').findAll('audio'),
+      notices: this.get('store').findAll('notice'),
+      church: this.get('store').findRecord('church', 0),
+      services: this.get('store').findAll('service')
     });
   }
 });
