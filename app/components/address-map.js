f636c6a473a380ad7deb0f6899d3c0b1ccbdf59f
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-11 13:45:22 +1200

    set show hide for map

--- app/components/address-map.js
+++ app/components/address-map.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { and } from '@ember/object/computed';
 
 export default Component.extend({
+  hasLocation: and('church.latitude','church.longitude'),
   location: computed('church', function() {
     return [this.get('church.latitude'),this.get('church.longitude')];
   })
