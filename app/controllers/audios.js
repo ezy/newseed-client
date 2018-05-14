78158094ebf0e2ac1a1011f7998f4a0cbe519976
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-14 16:49:43 +1200

    add speaker filter

--- app/controllers/audios.js
+++ app/controllers/audios.js
@@ -1,8 +1,31 @@
 import Controller from '@ember/controller';
 import { filter } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+
+  queryParams: ['speaker'],
+  speaker: null,
+
   filteredAudio: filter('model', (audio) => {
     return audio.get('status') === 'published';
   }),
+
+  speakerAudio: computed('speaker', 'filteredAudio', function() {
+    let speaker = this.get('speaker'),
+        audios = this.get('filteredAudio');
+
+    if (speaker) {
+      audios = audios.filter(audio => {
+        return audio.get('speaker').includes(speaker);
+      });
+    }
+
+    return audios.sortBy("date").reverse();
+  }),
+  actions: {
+    resetQuery() {
+      this.set('speaker', null);
+    }
+  }
 });
