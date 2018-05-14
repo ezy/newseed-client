ba26f9943c86e12cd6b8d9c97943db899da92fb2
Ezra Keddell <ezrakeddell@gmail.com>
2018-05-14 19:12:33 +1200

    restyle address foot and setup image links

--- app/mixins/church-set-controller.js
+++ app/mixins/church-set-controller.js
@@ -6,8 +6,12 @@ export default Mixin.create({
     this.get('store').findRecord('church', 0).then(function(church) {
       controller.set('church', church);
     });
-    this.get('store').findAll('page').then(function(page) {
-      controller.set('pages', page);
+    this.get('store').findAll('page').then(function(pagesResult) {
+      let res = pagesResult.filter(page => {
+        console.log(page.get('status'));
+        return page.get('status') === 'published';
+      });
+      controller.set('pages', res);
     });
   }
 });
