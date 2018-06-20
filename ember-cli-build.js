3fd5440d460b723bee7ac3ea54047a4283ebfb4c
Ezra Keddell <ezrakeddell@gmail.com>
2018-06-20 16:09:28 +1200

    leaflet assets no fingerprint

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -12,7 +12,14 @@ module.exports = function(defaults) {
       theme: 'airbnb'
     },
     fingerprint: {
-      exclude: ['assets/img']
+      exclude: [
+      'assets/img',
+      'images/layers-2x.png',
+      'images/layers.png',
+      'images/marker-icon-2x.png',
+      'images/marker-icon.png',
+      'images/marker-shadow.png'
+    ]
     }
   });
 
