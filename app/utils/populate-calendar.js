7e6dfc1d32324234e1ce16b0485f555b27a250a5
Ezra Keddell <ezrakeddell@gmail.com>
2018-07-15 08:47:05 +1200

    stop both sundays showing in cal

--- app/utils/populate-calendar.js
+++ app/utils/populate-calendar.js
@@ -9,7 +9,7 @@ export default EmberObject.extend({
       const dateDayName = moment(e.get('date')).format('dddd');
       const monday = moment().day(1).startOf('day');
       const comingSunday = moment().day(7).endOf('day');
-      // const thisSunday = moment().day(0).endOf('day');
+      const thisSunday = moment().day(0).endOf('day');
       if (freq === 'day') {
         const weekDays = 7;
         // Set numeral for weekday from moment isoWeekday
@@ -50,8 +50,13 @@ export default EmberObject.extend({
         if (!e.get('title') === 'Service') {
           sched.get(dateDayName).pushObject(e);
         }
-        // Only push services to array before coming Sunday, end of day
-        else if (date.isBefore(comingSunday)) {
+        /**
+         * Only push services to array before coming Sunday, end of day.
+         * // NOTE: On a Sunday, the date.isBefore(thisSunday) needs to be set
+         * to avoid both this Sunday and next Sunday showing on the event
+         * schedule (which was happening with [comingSunday]).
+         */
+        else if (date.isBefore(thisSunday)) {
           sched.get(dateDayName).pushObject(e);
         }
       }
