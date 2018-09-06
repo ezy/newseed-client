8ee8b3ed9e578165805cdc84d726c9475a5650dc
Ezra Keddell <ezrakeddell@gmail.com>
2018-09-06 16:47:25 +1200

    fix general events and duplicate sundays

--- app/utils/populate-calendar.js
+++ app/utils/populate-calendar.js
@@ -4,12 +4,13 @@ import moment from 'moment';
 export default EmberObject.extend({
   setData(data, sched) {
     data.forEach(e => {
+      const today = moment();
       const freq = e.get('frequency');
       const date = moment(e.get('date'));
       const dateDayName = moment(e.get('date')).format('dddd');
+      const thisSunday = moment().day(0).endOf('day');
       const monday = moment().day(1).startOf('day');
       const comingSunday = moment().day(7).endOf('day');
-      const thisSunday = moment().day(0).endOf('day');
       if (freq === 'day') {
         const weekDays = 7;
         // Set numeral for weekday from moment isoWeekday
@@ -30,6 +31,9 @@ export default EmberObject.extend({
           sched.get(dayName).pushObject(e);
         }
       }
+      else if (freq === 'week') {
+        return sched.get(dateDayName).pushObject(e);
+      }
       else if (freq === 'fortnight') {
         const dateWeekParity = moment(date).week() % 2;
         const thisWeekParity = moment().week() % 2;
@@ -45,23 +49,22 @@ export default EmberObject.extend({
           ? sched.get(dateDayName).pushObject(e)
           : null ;
       }
-      else {
-        // Push all events to array
-        if (!e.get('title') === 'Service') {
-          return sched.get(dateDayName).pushObject(e);
+      else if (date.isBetween(monday, comingSunday)) {
+        if (e.get('title') === 'Service') {
+          /**
+           * Only push services to array before coming Sunday, end of day.
+           * // NOTE: On a Sunday, the date.isBefore(thisSunday) needs to check
+           * today is not Sunday to avoid both this Sunday and next Sunday
+           * showing on the event schedule.
+           */
+          if (date.isBefore(thisSunday) && !today.isSame(thisSunday, 'day')) {
+            return sched.get(dateDayName).pushObject(e);
+          }
+          else if (date.isBefore(comingSunday)) {
+            return sched.get(dateDayName).pushObject(e);
+          }
         }
-        /**
-         * Only push services to array before coming Sunday, end of day.
-         * // NOTE: On a Sunday, the date.isBefore(thisSunday) needs to be set
-         * to avoid both this Sunday and next Sunday showing on the event
-         * schedule (which was happening with [comingSunday]).
-         */
-        else if (date.isBefore(thisSunday)) {
-          return sched.get(dateDayName).pushObject(e);
-        }
-        else if (date.isBefore(comingSunday)) {
-          return sched.get(dateDayName).pushObject(e);
-        }
+        return sched.get(dateDayName).pushObject(e);
       }
     });
   }
